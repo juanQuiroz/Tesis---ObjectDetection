489f9618ecf5218fa0a5f7781e6bfb2c01df45ae
Juan de Dios <jaqp_911@hotmail.com>
2022-02-13 15:11:23 -0500

    first commit

---
+++ src/utilities.js
@@ -0,0 +1,20 @@
+export const drawRect = (detections, ctx) => {
+  // Obtener los resultados de las predicciones
+
+  detections.forEach(prediction => {
+    const [x, y, width, height] = prediction["bbox"];
+    const text = prediction["class"];
+
+    // definiendo estilios
+    const color = "green";
+    ctx.strokeStyle = color;
+    ctx.font = "16px Arial";
+    ctx.fillStyle = color;
+
+    // Dibujan los rectangulos y texto
+    ctx.beginPath();
+    ctx.fillText(text, x, y);
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
+  });
+};
